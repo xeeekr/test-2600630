11539b288c9c078ce64e4d5aa8b869f45f872747
xeeekr <xeeekr@gmail.com>
2026-06-30 14:53:46 +0900

    init

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,5 @@
 import { defineConfig } from "vite";
 export default defineConfig({
-  server: {
-    port: 5173,
-    host: true,
-  },
-  build: {
-    outDir: "dist",
-  },
+  server: { port: 5173, host: true },
+  build: { outDir: "dist" },
 });
